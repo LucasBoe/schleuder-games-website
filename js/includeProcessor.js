d2d0ece0d09238f64d74bdb41323ca493d64a6ad
LucasBoe <lucas@lumotex.de>
2024-01-28 14:15:47 +0100

    cleanup and fixes + animated hills and animated lines style adaption

--- js/includeProcessor.js
+++
@@ -1,8 +0,0 @@
-$(() => {
-    $("include").each(function () {
-        var data = $(this).attr("data");
-        ($(this).load(data, function () {
-            $(this).children().unwrap();
-        }));
-    });
-})
